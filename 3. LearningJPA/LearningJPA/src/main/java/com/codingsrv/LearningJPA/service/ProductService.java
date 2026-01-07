59ee0a65dfbd40f4af1f87a7b61215e1a3e23979
code-by-srv <srvsharma09@gmail.com>
2026-01-07 16:58:28 +0530

    creating a JPA entity for products and defining constraints + indexes

---
+++ 3. LearningJPA/LearningJPA/src/main/java/com/codingsrv/LearningJPA/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.codingsrv.LearningJPA.service;
+
+public class ProductService {
+}
---
+++ 3. LearningJPA/LearningJPA/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into product_catalog.product_table
+(price,quantity,sku,title)
+values
+(15.40,4,'parle','parleG'),
+(25.40,5,'pepsi','zero_pepsi'),
+(55.40,8,'cococola','diet_coke'),
+(45.40,12,'campa','diet_campa');
