e230d0be9c9b2ac9f13fbdccd729c242f53e182c
code-by-srv <srvsharma09@gmail.com>
2026-01-27 20:27:13 +0530

    annotations, Bean & DI

--- 3. LearningJPA/LearningJPA/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into product_catalog.product_table
-(price,quantity,title,sku)
- values
- (15.40, 4, 'parle', 'parleG'),
- (25.40, 5, 'pepsi', 'zero_pepsi'),
- (55.40, 8, 'cococola', 'diet_coke'),
- (45.40, 12, 'campa', 'diet_campa');
